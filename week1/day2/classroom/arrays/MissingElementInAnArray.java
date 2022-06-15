10781fe6384a5348452ffb40cc80a4a142cf8259
gpri11 <107539425+gpri11@users.noreply.github.com>
2022-06-15 13:23:09 +0530

    Update MissingElementInAnArray.java

--- week1/day2/classroom/arrays/MissingElementInAnArray.java
+++ week1/day2/classroom/arrays/MissingElementInAnArray.java
@@ -5,6 +5,14 @@ public class MissingElementInAnArray {
 	public static void main(String[] args) {
 		// Here is the input
 		int[] arr = {1,2,3,4,7,6,8};
+		
+		for (int i = arr[0]; i < arr.length ; i++)
+		{
+			if(i!= arr[i-1])
+			{
+				system.out.println(i);
+			}
+		}
 
 		// Sort the array	
 		
